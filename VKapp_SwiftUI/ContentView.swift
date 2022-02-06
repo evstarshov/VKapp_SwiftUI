8789895c48555c07bf16ccc1338d1c0b88a7d2d1
EugeneStarshov <yeenzo@yandex.ru>
2022-02-06 13:08:48 +0300

    Initial Commit

---
+++ VKapp_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VKapp_SwiftUI
+//
+//  Created by Евгений Старшов on 06.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VKapp_SwiftUI/VKapp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKapp_SwiftUIApp.swift
+//  VKapp_SwiftUI
+//
+//  Created by Евгений Старшов on 06.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKapp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
