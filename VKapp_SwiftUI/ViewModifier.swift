9575fab6d23365fb8821cca1d4507d5a3017b2b6
EugeneStarshov <yeenzo@yandex.ru>
2022-02-10 16:00:55 +0300

    Custom View, view modifier, view builder

---
+++ VKapp_SwiftUI/ViewModifier.swift
@@ -0,0 +1,25 @@
+//
+//  ViewModifier.swift
+//  VKapp_SwiftUI
+//
+//  Created by Евгений Старшов on 10.02.2022.
+//
+
+import SwiftUI
+
+
+
+struct CircleShadow: ViewModifier {
+
+    let shadowColor: Color = .black
+    let shadowRadius: CGFloat = 5
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 80, height: 80)
+            .cornerRadius(100)
+            .background(Circle()
+                .fill(Color.white)
+                .shadow(color: shadowColor, radius: shadowRadius))
+    }
+}
